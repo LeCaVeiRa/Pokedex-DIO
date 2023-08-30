0ab3d8e1688976456648d0236ebe2215211ba135
LeCaVeiRa <39318329+LeCaVeiRa@users.noreply.github.com>
2023-08-30 15:59:33 -0300

    Iniciando a Manipulacai de API e adicionando pokemons

---
+++ assets/js/main.js
@@ -0,0 +1,28 @@
+function convertPokemonTypesHtml(pokemonTypes){
+    return pokemonTypes.map((typeSlot) => `<li class="type">${typeSlot.type.name}</li>` )
+}
+
+function convertPokemonHtml(pokemon){
+    return`
+    <li class="pokemon">
+        <span class="number">#${pokemon.order}</span>
+        <span class="name">${pokemon.name}</span>
+
+        <div class="detail">
+            <ol class="types">
+                ${convertPokemonTypesHtml(pokemon.types).join('')}
+            </ol>
+
+            <img src="${pokemon.sprites.other.dream_world.front_default}"
+            alt="${pokemon.name}">
+        </div>
+    </li>
+    `
+}
+
+const pokemonList = document.getElementById('pokemonList')
+
+pokeApi.getPokemons().then((pokemons = []) => {
+    pokemonList.innerHTML += pokemons.map(convertPokemonHtml).join('')
+})
+.catch((err)=> console.error(err));
